1ffbf5a58b4a3ba02547927ed9979e51b9c7a8b8
Connor Adamy <cnadamy@gmail.com>
2023-11-08 13:10:56 -0700

    getenvTest

--- forum.php
+++ forum.php
@@ -2,10 +2,16 @@
 
 <?php 
 session_start();
+/*
 $sname = "dfkpczjgmpvkugnb.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
 $uname = "tlebx5gnlgzv2jae";
 $password = "yzb3u2dvou6cmwwu";
 $dbName = "i9t5zecos08o6qyz";
+*/
+$sname = getenv('DB_HOST');
+$uname = getenv('DB_USERNAME');
+$password = getenv('DB_PASSWORD');
+$dbName = getenv('DB_NAME');
 $conn = mysqli_connect($sname, $uname, $password, $dbName);
 
 if (!isset($_SESSION['username'])) {
