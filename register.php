454a5bc1f8e4ee775e8f5db809f20708d8020060
Connor Adamy <cnadamy@gmail.com>
2023-11-27 20:37:19 -0700

    jQueryAttempt1

--- register.php
+++ register.php
@@ -5,6 +5,7 @@
         <meta name="viewport" content="width=device-width, intial-scale=1.0">
         <link rel="stylesheet" href="home.css">
         <link rel="icon" type="image/jpg" href="backGrounds/favicon.jpg" sizes="16x16">
+        <script src="jquery-3.7.1.min.js"></script>
     </header>
     <body>
         <nav class="header">
@@ -40,6 +41,22 @@
                 <hr class="hrPad">
                 <input type="password" id="reTypePassword" name="re_password" class="loginField" placeholder="retype password">
                 <hr class="hrPad">
+
+                <script>
+                    $(document).ready(function () {
+                        $('#reTypePassword').on('input', function () {
+                            var password = $('password').val();
+                            var reTypePassword = $(this).val();
+
+                            if (password === reTypePassword) {
+                                $('#passwordMatch').text('');
+                            } else {
+                                $('#passwordMatch').text('Passwords do not match');
+                            }
+                        });
+                    });
+                </script>
+
                 <button type="submit" class="send">Register</button> 
             </form>
             <hr class="hrPad">
